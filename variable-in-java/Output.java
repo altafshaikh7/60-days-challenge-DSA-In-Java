98deab8a79e91aa2e3a257a5c9d0a14420d3a9cb
altafshaikh7 <altafshaikh07781@gmail.com>
2026-06-14 17:26:02 +0530

    output

---
+++ variable-in-java/Output.java
@@ -0,0 +1,14 @@
+
+public class Output {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.print("Hello, World!\n");
+        System.out.println("Hello, World!");
+
+    }
+
+}
+
+// Output
+// Hello, World!
