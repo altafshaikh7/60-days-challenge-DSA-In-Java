d6fd863ce63863f87024ad66edbdab33de4c0db4
altafshaikh7 <altafshaikh07781@gmail.com>
2026-06-14 20:00:04 +0530

    data types

---
+++ variable-in-java/Variables.java
@@ -0,0 +1,20 @@
+public class Variables {
+    public static void main(String[] args) {
+        //Types of Variables in Java
+        // Primitive Data Types
+        boolean isJavaFun = true;
+        char grade = 'A';
+        byte age = 25;
+        short population = 32000;
+        int salary = 50000;
+        long worldPopulation = 8000000000L;
+        float temperature = 36.5f;
+        double pi = 3.14159265359;
+
+        // Non-Primitive Data Types
+        String name = "John Doe";
+        Object obj = new Object();
+        int[] numbers = {1, 2, 3, 4, 5};
+
+    }
+}
