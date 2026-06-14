b061dc8395d3d58c4df118f9d10165cebf203069
altafshaikh7 <altafshaikh07781@gmail.com>
2026-06-14 16:51:44 +0530

    theory

---
+++ intro/theory.java
@@ -0,0 +1,43 @@
+// Java is a high-level, object-oriented programming language developed by Oracle Corporation (originally by Sun Microsystems). It is widely used for:
+
+// Web applications
+// Mobile apps (especially Android)
+// Desktop software
+// Enterprise systems
+// Cloud and backend development
+
+// Java programs can run on any device that has the Java Virtual Machine (JVM).
+
+// Features of Java:
+// Simple – Easy to learn and understand.
+// Object-Oriented – Based on objects and classes.
+// Platform Independent – Runs on different operating systems.
+// Secure – Provides strong security features.
+// Robust – Handles errors effectively.
+// Multithreaded – Supports multiple tasks at the same time.
+// Portable – Easy to move between systems.
+
+// class → Defines a class.
+// main() → Starting point of the program.
+// System.out.println() → Prints output on the screen.
+
+// How Java Works
+// Write Java code (.java file)
+// Compile using Java Compiler (javac)
+// Bytecode (.class file) is generated
+// JVM executes the bytecode
+
+
+// Java Components
+// JDK – Java Development Kit
+// JRE – Java Runtime Environment
+// JVM – Java Virtual Machine
+
+
+// Operators in Java
+// | Operator Type | Example     |   |    |
+// | ------------- | ----------- | - | -- |
+// | Arithmetic    | `+ - * / %` |   |    |
+// | Relational    | `== != > <` |   |    |
+// | Logical       | `&&         |   | !` |
+
