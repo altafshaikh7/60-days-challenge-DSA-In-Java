9cc5bbde99b252094778e6865830022feda95ad1
altafshaikh7 <altafshaikh07781@gmail.com>
2026-06-15 16:45:07 +0530

    Question 1

---
+++ Conditional-Statement/question_1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class question_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int b = scanner.nextInt();
+
+        if (a == b) {
+            System.out.println("Both numbers are equal.");
+        } else if (a > b) {
+            System.out.println("First number is greater than second number.");
+        } else {
+            System.out.println("Second number is greater than first number.");
+        }
+    }
+
+}
