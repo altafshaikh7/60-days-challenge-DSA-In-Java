83f1690dea63db73938e80d4466e7c2afe3aa38a
altafshaikh7 <altafshaikh07781@gmail.com>
2026-06-15 16:05:30 +0530

    if else condition

---
+++ Conditional-Statement/lecture_1.java
@@ -0,0 +1,12 @@
+
+public class lecture_1 {
+
+    public static void main(String[] args) {
+        int x = 10;
+        if (x > 5) {
+            System.out.println("x is greater than 5");
+        } else {
+            System.out.println("x is not greater than 5");
+        }
+    }
+}
