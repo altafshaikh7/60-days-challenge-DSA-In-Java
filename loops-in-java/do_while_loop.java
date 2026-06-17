3987c218ba68473ba8b5ad27dd60a27119f7c705
altafshaikh7 <altafshaikh07781@gmail.com>
2026-06-17 17:33:20 +0530

    Do-while loop

--- loops-in-java/do_while_loop.java
+++ loops-in-java/do_while_loop.java
@@ -16,6 +16,6 @@ public class do_while_loop {
             System.out.print("Altaf Shaikh\n"); // yeh print hoga kyuki do-while loop me pehle body execute hoti hai fir
                                                 // condition check hoti hai
             i++;
-        } while (i < 11); // do while loop me terminal hoti hai isliye lagana padega
+        } while (i < 11)// do while loop me terminal hoti hai isliye lagana padega
     }
 }
