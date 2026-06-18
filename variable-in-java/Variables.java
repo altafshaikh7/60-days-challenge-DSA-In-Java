66a0227c1e65086453696b9c98c197f699bac928
altafshaikh7 <altafshaikh07781@gmail.com>
2026-06-18 17:50:51 +0530

    variable

--- variable-in-java/Variables.java
+++ variable-in-java/Variables.java
@@ -1,20 +1,12 @@
 public class Variables {
     public static void main(String[] args) {
-        //Types of Variables in Java
-        // Primitive Data Types
-        boolean isJavaFun = true;
-        char grade = 'A';
-        byte age = 25;
-        short population = 32000;
-        int salary = 50000;
-        long worldPopulation = 8000000000L;
-        float temperature = 36.5f;
-        double pi = 3.14159265359;
 
-        // Non-Primitive Data Types
-        String name = "John Doe";
-        Object obj = new Object();
-        int[] numbers = {1, 2, 3, 4, 5};
+        int x = 5; // initiolization + declaration
+        System.out.println(x);
+
+        int y; // initiolization
+        y = 10; // declaration
+        System.out.println(y);
 
     }
 }
