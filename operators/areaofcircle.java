cb68f734dbcf312fd9e7fa256a98414dfeecd573
altafshaikh7 <altafshaikh07781@gmail.com>
2026-06-19 09:51:45 +0530

    use Scanner for input

--- operators/areaofcircle.java
+++ operators/areaofcircle.java
@@ -1,15 +1,26 @@
 package operators;
 
+import java.util.Scanner;
+
 public class areaofcircle {
     public static void main(String[] args) {
-
-        double r = 3.8;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The Radius: ");
+        double r = sc.nextDouble();
         double a = 3.14 * r * r;
-        System.out.println(a);
+        System.out.println("Area is " + a);
 
         // HomeWork
         double v = 3.8;
         double volume = (4.0 / 3.0) * 3.14 * r * r * r;
         System.out.println(volume);
+
+        Scanner x = new Scanner(System.in);
+        System.out.println("Enter the Number");
+
+        int b = x.nextInt();
+        int y = b * b;
+        System.out.println("the square of the number is " + y);
+
     }
 }
