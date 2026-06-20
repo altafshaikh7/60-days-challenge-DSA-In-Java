c304da3e8d1db3414500b1759f89420d2f872710
altafshaikh7 <altafshaikh07781@gmail.com>
2026-06-20 18:14:38 +0530

    division of data types

---
+++ intro/divisionofdata.java
@@ -0,0 +1,22 @@
+public class divisionofdata {
+    public static void main(String[] args) {
+
+        int a = 10; // int and double = double
+        double b = 3;
+
+        System.out.println(a / b);
+
+        int c = 10; // int and int = int
+        int d = 3;
+        System.out.println(c / d);
+
+        int e = 10; // int and double = double
+        double f = 3.0;
+        System.out.println(e / f);
+
+        double g = 10.0; // double and int = double
+        int h = 3;
+        System.out.println(g / h);
+
+    }
+}
